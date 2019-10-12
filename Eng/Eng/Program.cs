af00f3e37bb7c599016fa2a758ef65b26868def0
Sirada Soonthorn <ploy_puth@outlook.co.th>
2019-10-12 11:20:18 +0700

    first year semester 1

--- Eng/Eng/Program.cs
+++ Eng/Eng/Program.cs
@@ -11,6 +11,8 @@ namespace Eng
         static void Main(string[] args)
         {
             Console.WriteLine("Year 255x semeater 1, I am an Engdu freahman");
+            Console.WriteLine("Year 255x semeter 2, I Learn to program in C sharp.");
+
 
         }
     }
